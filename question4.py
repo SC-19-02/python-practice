c6ec96be774c188c54af3ce0838925fe5c591072
SC-19-02 <sheel.chudasama@strathmore.edu>
2025-04-24 18:48:27 +0300

    commit for question 4

---
+++ question4.py
@@ -0,0 +1,16 @@
+def reverse_string():
+    # empty string to store the reversed string
+    reversed_string = ""
+
+    # get the string from the user
+    input_string = input("Enter a string to reverse: ")
+
+    # for loop loops through the input string from the end to the start
+    for i in range(len(input_string) - 1, -1, -1):
+        # adds each character to the reversed string
+        reversed_string += input_string[i]
+
+    return reversed_string
+
+# function call
+print(reverse_string())
