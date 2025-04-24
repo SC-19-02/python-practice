6d63801fc3668d381a0b8cf0388cf8f1c2750c42
SC-19-02 <sheel.chudasama@strathmore.edu>
2025-04-24 17:51:23 +0300

    commit for question 1

---
+++ question1.py
@@ -0,0 +1,13 @@
+# function that takes a list of numbers as input
+def sum_elements(numbers):
+    # total is set to 0 at the start
+    total = 0
+    # for loop loops through each number in the list
+    for number in numbers:
+        # adds the current number to the total
+        total += number
+    # total is returned at the end
+    return total
+
+# function call
+print(sum_elements([1, 2, 3, 4, 5]))
